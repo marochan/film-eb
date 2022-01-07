34dcef2a111f129becfff022a2d1cf78936b5c61
Cezary Rokita <65018003+marochan@users.noreply.github.com>
2022-01-07 21:35:38 +0100

    Konwersja tokenu na id uzytkonika

--- data-access-service/data-access-service/src/main/java/com/filmlebendczil/dataaccessservice/controller/DataAccessController.java
+++ data-access-service/data-access-service/src/main/java/com/filmlebendczil/dataaccessservice/controller/DataAccessController.java
@@ -99,19 +99,33 @@ public class DataAccessController {
 
 
 		ObjectMapper mapper = new ObjectMapper();
-		MovieRating rating = mapper.readValue(ratingString, new TypeReference<MovieRating>() {
-		});
-
-		Long id = rating.getMovieId();
-		Long userId = rating.getMemberId();
-		
-		
+		Map<String, Object> request = mapper.readValue(ratingString, new TypeReference<Map<String, Object>>() {
+		});			
+		//token po którym szukamy uzytkownika
+		String value =(String) request.get("token");
+		//id uzytkownika ktore jest dodawane do movie rating
+		Long memberId = memberRepo.findByUserId(value).getID();
+			
+		Long movieId = (Long)request.get("movieId");
+		MovieRating rating = new MovieRating();
+		rating.setMovieId(movieId);
+		rating.setMemberId(memberId);
+		rating.setCreateDate((Date)request.get("createDate"));
+		rating.setCat1((double)request.get("cat1"));
+		rating.setCat2((double)request.get("cat2"));
+		rating.setCat3((double)request.get("cat3"));
+		rating.setCat4((double)request.get("cat4"));
+		rating.setCat5((double)request.get("cat5"));
+		rating.setCat6((double)request.get("cat6"));
+		rating.setScore((double)request.get("score"));
+			
+			
 		movieRatingRepo.save(rating);
 
-		Optional<Movie> movie = movieRepo.findById(id);
+		Optional<Movie> movie = movieRepo.findById(movieId);
 		Movie toUpdate = movie.get();
 
-		List<MovieRating> allRatings = movieRatingRepo.findAllByMovieId(id);
+		List<MovieRating> allRatings = movieRatingRepo.findAllByMovieId(movieId);
 		int counter = 0;
 		double sum1 = 0.0;
 		double sum2 = 0.0;
@@ -129,12 +143,14 @@ public class DataAccessController {
 			sum5 += mr.getCat5();
 			sum6 += mr.getCat6();
 
-		}
+			}
 
 		toUpdate.setRatings(counter, sum1, sum2, sum3, sum4, sum5, sum6);
 		movieRepo.save(toUpdate);
-		updateUserProfile(userId);
-	}
+		updateUserProfile(memberId);
+		}
+	
+	
 	// aktualizacja profilu uzytkownika
 	public void updateUserProfile(Long userId) throws JsonProcessingException {
 		List<MovieRating> ratingByUser = movieRatingRepo.findAllByMemberId(userId);
